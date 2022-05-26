6e42eeb47dec36499a395eb08ac0da90eda739f7
Ashwin Rohit <aalagiri@ucsd.edu>
2022-05-25 21:05:08 -0700

    StringAlgorithms: Moved implementations from header to Source file.

--- Sources/StringAlgorithms.cpp
+++ Sources/StringAlgorithms.cpp
@@ -4,27 +4,28 @@
 namespace StringAlgorithms {
     using namespace std;
 
-    string Substring(const string& str, usize begin, usize end = string::npos) {
+    string Substring(const string &str, usize begin, usize end = string::npos) {
         usize str_size = str.size();
-        if(end > str_size) end = str_size;
+        if (end > str_size) end = str_size;
+        // TODO: Implement Substring
         return string();
     }
 
-    vector<string> Split(const string &haystack, const string& needle) {
+    vector<string> Split(const string &haystack, const string &needle) {
         vector<string> wheat;
         usize begin = 0;
         usize end = 0;
         usize haystack_size = haystack.size();
-        while(end < haystack_size) {
+        while (end < haystack_size) {
             end = haystack.find(needle, begin);
-            if(end == string::npos) {
+            if (end == string::npos) {
                 // end = npos
                 // => npos = max(usize)
                 // => substr will be the last remaining string
                 wheat.push_back(haystack.substr(begin, end));
                 break;
             }
-            wheat.push_back(haystack.substr(begin, end-begin));
+            wheat.push_back(haystack.substr(begin, end - begin));
             end += needle.size();
             begin = end;
         }
@@ -36,19 +37,15 @@ namespace StringAlgorithms {
         if (str_len <= 0) return "";
         isize rot_amount = rotate_num % str_len;
         if (rot_amount == 0) {
-            return
-                    str;
+            return str;
         } else {
             if (rot_amount > 0) {
-                return str.
-                        substr(str_len
-                               - rot_amount, string::npos)
+                return str.substr(str_len- rot_amount, string::npos)
                        + str.substr(0, str_len - rot_amount);
             } else {
                 rot_amount *= -1;
-                return str.
-                        substr(rot_amount, string::npos
-                ) + str.substr(0, rot_amount);
+                return str.substr(rot_amount, string::npos)
+                        + str.substr(0, rot_amount);
             }
         }
     }
